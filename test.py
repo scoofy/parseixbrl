822c55f42714e23128dcde11981bb18e8210612d
scoofy <mschoolfield@gmail.com>
2018-01-19 16:51:51 -0800

    okay, most basic testing copy done

--- test.py
+++ test.py
@@ -43,22 +43,20 @@ def zip_contents(zipfile):
 
 def simple_parse_xbrl(zipfile):
     ticker = None
-    logging.info(zipfile)
+    # logging.info(zipfile)
     archive = zip_contents(zipfile)
     name_list = archive.namelist()
     main_file_name = None
     for name in name_list:
         if name.endswith(".xml") and "_" not in name:
-            logging.info(name)
+            # logging.info(name)
             ticker = name.split("-")[0].upper()
-            print("ticker: " + ticker)
             main_file_name = name
 
     ns = {}
     for event, (name, value) in ET.iterparse(archive.open(main_file_name), ['start-ns']):
         if name:
             ns[name] = value
-    # pp.pprint(ns)
 
     tree = ET.parse(archive.open(main_file_name))
     root = tree.getroot()
@@ -67,7 +65,6 @@ def simple_parse_xbrl(zipfile):
         period = {}
         fact_dict = {}
         for item in element.iter():
-            print(item.tag)
             if "identifier" in item.tag:
                 if "scheme" in item.attrib.keys():
                     scheme = item.attrib.get("scheme")
@@ -111,64 +108,9 @@ def simple_parse_xbrl(zipfile):
             # context_element_dict["period"] = period
 
             fact_dict[context_id_short + ":" + short] = context_element_dict
-            if context_element_dict:
-                break
-        ticker_dict = {ticker: fact_dict}
-        # print("\n"*10)
-        pp.pprint(ticker_dict)
-        sys.exit()
-    # rando = tree[100]
-    # print(rando.attrib)
-    # print(rando.attrib.keys())
-    # for element in rando.iter():
-    #     print(element)
-    #     print(element.attrib)
-    #     print(element.attrib.keys())
-    # for element in tree.iter():
-    #     pp.pprint(element.attrib)
-    #     if "unitref" in element.attrib.keys():
-    #         print("\t\t" + str(element))
-    #         print("\t" + str(element.text))
-    #         for subelement in element.iter():
-    #             print(subelement)
-    sys.exit()
-    the_xml = archive.read(main_file_name)
-    soup = BeautifulSoup(the_xml, "lxml")
-    data_list = []
-    attribute_set = set([])
-    contextref_set = set([])
-    everything_list = soup.find_all()
-    tag_list = [tag.name for tag in everything_list]
-    tag_set = set(tag_list)
-    #logging.info(tag_set)
-    single_element_list = [element for element in everything_list if ":" in element.name and tag_list.count(element.name)==1] #or use defaultdict from the collections,if perfomance matters:
-    count = 0
-    for element in single_element_list:
-        if len(str(element.string)) < 100:
-            print(element.name)
-            print(element.string)
-            count+=1
-        else:
-            pass
-            #print(element.name)
-            #pp.pprint(element.string)
-    print(len(single_element_list))
-    print(count)
 
-    # id_num_list = soup.find_all(id=re.compile("^ID_"))
-    # fact_hash_list = soup.find_all(id=re.compile("^Fact-"))
-    # all_the_shit = id_num_list + fact_hash_list
-    # logging.info("len(all_the_shit): {}".format(len(all_the_shit)))
-    # for xbrl_entity in all_the_shit:
-    #     name = xbrl_entity.name
-    #     contents = xbrl_entity.contents
-    #     attr_dict = xbrl_entity.attrs
-    #     attr_dict['contents'] = contents
-    #     my_dict = {"name": attr_dict}
-    #     data_list.append(my_dict)
-    #     attribute_set.add(xbrl_entity.name)
-    #     contextref_set.add(xbrl_entity.get("contextref"))
-    # return attribute_set, contextref_set
+        ticker_dict = {ticker: fact_dict}
+        return(ticker_dict)
 
 files = ["aro111.zip",
          "ge10.zip",
@@ -177,12 +119,23 @@ files = ["aro111.zip",
          "ge73.zip",
         ]
 
+
+from itertools import islice
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
+number_of_items = 5
 start = time.time()
 logging.info(start)
 
+for file in files:
+    stock_dict = simple_parse_xbrl(file)
+    ticker = list(stock_dict.keys())[0]
+    data_dict = list(stock_dict.values())[0]
+    some_items = dict(take(number_of_items, data_dict.items()))
+    pp.pprint({ticker: some_items})
 
-simple_parse_xbrl(files[0])
-sys.exit()
 
 stop = time.time()
 logging.info(stop)
